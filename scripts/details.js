2e8edc91ca7142c75f14df3fc99a7975295c6761
Noel Guillen <noelblas274@gmail.com>
2024-03-04 21:16:50 -0500

    added starter code

---
+++ scripts/details.js
@@ -0,0 +1,37 @@
+    const productHeader = document.getElementById("product-header");
+    const productHeaderLink = document.getElementById('product-header-link');
+    const productItemName = document.getElementById('product-item-name');
+    const productItemPrice = document.getElementById('product-item-price');
+    const productItemDesc = document.getElementById('product-item-desc');
+    const productItemColor = document.getElementById('product-item-color');
+    const productItemSizeContainer = document.getElementById('product-item-size-container');
+    const productIdNumber = document.getElementById('product-id-number');
+    const productCategoryName = document.getElementById('product-category-name');
+    const productTagsNameContainer = document.getElementById('product-tags-name-container');
+    const tabs-product-des = document.getElementById('tabs-product-des');
+
+    // let currentId;
+
+    // const urlParams = new URLSearchParams(window.location.search);
+    // currentId = urlParams.get("q");
+
+    // const url = `https://affogato-the-ecommerce-store.p.rapidapi.com/api/items/${currentId}`;
+    // const options = {
+    //     method: 'GET',
+    //     headers: {
+    //         'X-RapidAPI-Key': 'ad135d37ffmshca57b4f4ff275f0p153242jsnfaf4aaf1abb9',
+    //         'X-RapidAPI-Host': 'affogato-the-ecommerce-store.p.rapidapi.com'
+    //     }
+    // };
+
+    // fetch(url, options)
+    // .then(res => res.json())
+    // .then((json) => {
+    //     console.log(json.item);
+    // })
+    // .catch(err => console.log(err));
+
+
+    function displaySingleProduct(dataArray){
+
+    };
